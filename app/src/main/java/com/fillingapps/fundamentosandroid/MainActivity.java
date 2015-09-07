cff9bdff9f146c2cba2b5cc1a1e340a806e1cbde
Javi Alzueta <jalzuetap@gmail.com>
2015-09-07 22:56:16 +0200

    2 offline weather with Toggle

--- app/src/main/java/com/fillingapps/fundamentosandroid/MainActivity.java
+++ app/src/main/java/com/fillingapps/fundamentosandroid/MainActivity.java
@@ -5,8 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ToggleButton;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
     private ImageView mImageView;
 
@@ -16,23 +17,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         mImageView = (ImageView) findViewById(R.id.weather_image);
-        Button changeButton1 = (Button) findViewById(R.id.change_system1_button);
-        Button changeButton2 = (Button) findViewById(R.id.change_system2_button);
+        final ToggleButton changeButton1 = (ToggleButton) findViewById(R.id.change_system_button);
 
-        changeButton1.setOnClickListener(this);
-        changeButton2.setOnClickListener(this);
+        changeButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mImageView.setImageResource(changeButton1.isChecked()?R.drawable.offline_weather : R.drawable.offline_weather2);
+            }
+        });
 
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.change_system1_button:
-                mImageView.setImageResource(R.drawable.offline_weather);
-                break;
-            case R.id.change_system2_button:
-                mImageView.setImageResource(R.drawable.offline_weather2);
-                break;
-        }
-    }
 }
