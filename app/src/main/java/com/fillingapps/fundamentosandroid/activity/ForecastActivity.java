f5672faa6587cf6af341313f4cd675f19d3fb9a6
Javi Alzueta <jalzuetap@gmail.com>
2015-09-07 23:41:32 +0200

    Forecast model in ForecastActivity

--- app/src/main/java/com/fillingapps/fundamentosandroid/activity/ForecastActivity.java
+++ app/src/main/java/com/fillingapps/fundamentosandroid/activity/ForecastActivity.java
@@ -2,32 +2,45 @@ package com.fillingapps.fundamentosandroid.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.ImageView;
-import android.widget.ToggleButton;
+import android.widget.TextView;
 
 import com.fillingapps.fundamentosandroid.R;
+import com.fillingapps.fundamentosandroid.model.Forecast;
 
 public class ForecastActivity extends AppCompatActivity {
 
-    private ImageView mImageView;
+    private Forecast mForecast;
+
+    private ImageView mIcon;
+    private TextView mMaxTemp;
+    private TextView mMinTemp;
+    private TextView mHumidity;
+    private TextView mDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forecast);
 
-        mImageView = (ImageView) findViewById(R.id.weather_image);
-        final ToggleButton changeButton1 = (ToggleButton) findViewById(R.id.change_system_button);
+        mMaxTemp = (TextView) findViewById(R.id.max_temp);
+        mMinTemp = (TextView) findViewById(R.id.min_temp);
+        mHumidity = (TextView) findViewById(R.id.humidity);
+        mDescription = (TextView) findViewById(R.id.forecast_description);
+        mIcon = (ImageView) findViewById(R.id.forecast_image);
 
-        changeButton1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mImageView.setImageResource(changeButton1.isChecked()?R.drawable.offline_weather : R.drawable.offline_weather2);
-            }
-        });
+        setForecast(new Forecast(30,20,65,"Totalmente despejado",""));
 
     }
 
-    
+    public void setForecast(Forecast forecast){
+        mForecast = forecast;
+
+        mMaxTemp.setText(String.format(getString(R.string.max_temp_parameter), forecast.getMaxTemp()));
+        mMinTemp.setText(String.format(getString(R.string.min_temp_parameter), forecast.getMinTemp()));
+        mHumidity.setText(String.format(getString(R.string.humidity_parameter), forecast.getHumidity()));
+        mDescription.setText(String.format(forecast.getDescription()));
+    }
+
+
 }
