86a72bb99fe78a5a25d9e885ea391777fc27eb46
Javi Alzueta <jalzuetap@gmail.com>
2015-09-09 21:20:08 +0200

    Creamos Toolbar y la asignamos como Actionbar de la Activity principal

--- app/src/main/java/com/fillingapps/fundamentosandroid/activity/ForecastCityPagerActivity.java
+++ app/src/main/java/com/fillingapps/fundamentosandroid/activity/ForecastCityPagerActivity.java
@@ -3,6 +3,7 @@ package com.fillingapps.fundamentosandroid.activity;
 import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.fillingapps.fundamentosandroid.R;
 import com.fillingapps.fundamentosandroid.fragment.CityPagerFragment;
@@ -17,6 +18,10 @@ public class ForecastCityPagerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_forecast_city_pager);
+
+        // Asignamos la Toolbar como "ActionBar"
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         FragmentManager fm = getFragmentManager();
         if (fm.findFragmentById(R.id.fragment_citypager) == null){
